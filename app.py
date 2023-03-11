e01251d0d655ca2f9908d5ca274227ec4319b728
Clayton Errington <me@claytonerrington.com>
2023-03-11 15:13:52 -0600

    Updates for 2023

--- app.py
+++ app.py
@@ -6,26 +6,26 @@ app.config.from_pyfile('./config/config.cfg')
 
 @app.route('/')
 def home():
-    return render_template('default.html', date=app.config["EVENT_DATE"], title=app.config["TITLE"], annual=app.config["ANNUAL"], reglink=app.config["REG_LINK"])
+    return render_template('default.html', date=app.config["EVENT_DATE"], title=app.config["TITLE"], annual=app.config["ANNUAL"], reglink=app.config["REG_LINK"], park=app.config["EVENT_PARK"])
 
 
 @app.route('/contact/')
 def contact():
-    return render_template('contact.html', date=app.config["EVENT_DATE"], title=app.config["TITLE"], annual=app.config["ANNUAL"], reglink=app.config["REG_LINK"])
+    return render_template('contact.html', date=app.config["EVENT_DATE"], title=app.config["TITLE"], annual=app.config["ANNUAL"], reglink=app.config["REG_LINK"], park=app.config["EVENT_PARK"])
 
 
 @app.route('/404.html')
 def notfound():
-    return render_template('404.html', date=app.config["EVENT_DATE"], title=app.config["TITLE"], annual=app.config["ANNUAL"], reglink=app.config["REG_LINK"])
+    return render_template('404.html', date=app.config["EVENT_DATE"], title=app.config["TITLE"], annual=app.config["ANNUAL"], reglink=app.config["REG_LINK"], park=app.config["EVENT_PARK"])
 
 
 @app.route('/privacy/')
 def privacy():
-    return render_template('privacy.html', date=app.config["EVENT_DATE"], title=app.config["TITLE"], annual=app.config["ANNUAL"], reglink=app.config["REG_LINK"])
+    return render_template('privacy.html', date=app.config["EVENT_DATE"], title=app.config["TITLE"], annual=app.config["ANNUAL"], reglink=app.config["REG_LINK"], park=app.config["EVENT_PARK"])
 
 @app.route('/waiver/')
 def waiver():
-    return render_template('waiver.html', date=app.config["EVENT_DATE"], title=app.config["TITLE"], annual=app.config["ANNUAL"], reglink=app.config["REG_LINK"])
+    return render_template('waiver.html', date=app.config["EVENT_DATE"], title=app.config["TITLE"], annual=app.config["ANNUAL"], reglink=app.config["REG_LINK"], park=app.config["EVENT_PARK"])
 
 
 if __name__ == '__main__':
